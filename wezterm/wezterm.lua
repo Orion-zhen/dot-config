84934f740616b0b11c807f5b1d3e3d3cc13ce2f1
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-06-16 22:28:56 +0800

    :pencil2: appearance

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-local apperance = require("config.appearance")
+local appearance = require("config.appearance")
 local keymaps = require("config.keymaps")
 local colors = require("config.colors")
 local fonts = require("config.fonts")
 
-apperance.apply(config)
+appearance.apply(config)
 keymaps.apply(config)
 colors.apply(config)
 fonts.apply(config)
