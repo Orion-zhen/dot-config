c55185842768c96ee93d06a9046587fd2c3aeaa0
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-10-26 01:56:09 +0800

    :art: add new default chinese font

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -7,6 +7,7 @@ function module.apply(config)
     config.font = wezterm.font_with_fallback(
         {
             { family = "Maple Mono NF",    style = "Normal" },
+            { family = "霞鹜新晰黑＋",       style = "Normal" }, -- ttf-lxgw-neo-xiei-plus
             { family = "Hack Nerd Font",   style = "Normal" },
             { family = "Noto Sans CJK SC", style = "Normal" }
         }
