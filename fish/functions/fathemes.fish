a70c59e13b0b9f281a3f067a954fdb9991cbf8ed
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-10-27 12:34:15 +0800

    :fire: duplicate file

--- fish/functions/fathemes.fish
+++
@@ -1,9 +0,0 @@
-# bat 主题选择命令
-function bathemes --description "select bat theme interactively"
-    if test (count $argv) -ge 1
-        set file $argv[1]
-    else
-        set file "$HOME/.zshrc"
-    end
-    set -x BAT_THEME (bat --list-themes | fzf --preview "bat --theme={} --color=always $file")
-end
