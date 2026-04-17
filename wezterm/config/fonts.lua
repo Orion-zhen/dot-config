f3064f45f09fc51f40a02eb217011a74115706d0
Orion <https://github.com/Orion-zhen>
2026-04-18 01:50:58 +0800

    :wrench: use LXGWWenKai

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -6,10 +6,10 @@ function module.apply(config)
     -- 字体设置
     config.font = wezterm.font_with_fallback(
         {
-            { family = "Maple Mono NF",    style = "Normal" },
-            { family = "霞鹜新晰黑＋",       style = "Normal" }, -- ttf-lxgw-neo-xiei-plus
-            { family = "Hack Nerd Font",   style = "Normal" },
-            { family = "Noto Sans CJK SC", style = "Normal" }
+            { family = "Maple Mono NF",        style = "Normal" },
+            { family = "LXGWWenKai Nerd Font", style = "Normal" },
+            { family = "Hack Nerd Font",       style = "Normal" },
+            { family = "Noto Sans CJK SC",     style = "Normal" }
         }
     )
     config.font_size = 15.0
